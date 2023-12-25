4b6917f8d5ad0ae28e2fa86f695ba7701e1c3d33
naimur-234964 <nrsazib234964@gmail.com>
2023-12-25 23:46:34 +0600

    Login Completed

--- resources/views/front/account/login.blade.php
+++ resources/views/front/account/login.blade.php
@@ -19,14 +19,26 @@
                     {{ (Session::get('success')) }}
                 </div>
             @endif
+            @if (Session::has('error'))
+                <div class="alert alert-danger">
+                    {{ (Session::get('error')) }}
+                </div>
+            @endif
             <div class="login-form">
-                <form action="/examples/actions/confirmation.php" method="post">
+                <form action="{{ route('account.authenticate') }}" method="post">
+                    @csrf
                     <h4 class="modal-title">Login to Your Account</h4>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Email" required="required">
+                        <input type="text" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}">
+                        @error('email')
+                            <p class="invalid-feedback">{{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Password" required="required">
+                        <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password">
+                        @error('password')
+                            <p class="invalid-feedback">{{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="form-group small">
                         <a href="#" class="forgot-link">Forgot Password?</a>
